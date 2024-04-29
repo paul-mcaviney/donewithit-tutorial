c5cf8f20c73a2a98c38ec5067646e430cacd8b2b
paul-mcaviney <paul.mcaviney@protonmail.com>
2024-04-29 11:43:30 -0600

    Learning the basics

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello React Native!</Text>
+      <Image
+        fadeDuration={1000}
+        style={styles.image}
+        source={{ uri: "https://picsum.photos/200/300" }}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +21,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 300,
   },
 });
