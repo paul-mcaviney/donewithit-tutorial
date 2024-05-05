baea0efb7fe9d09b1a5c34db4fe92b9ad3c07592
paul-mcaviney <paul.mcaviney@protonmail.com>
2024-05-05 07:24:56 -0600

    Get Welcome Screen styled

--- App.js
+++ App.js
@@ -15,19 +15,28 @@ export default function App() {
       resizeMode="cover"
       style={styles.image}
     >
-      <Image source={require("./assets/logo-red.png")} style={styles.logo} />
-      <Text>Sell What You Don't Need</Text>
-      <View>
-        <Button title="Login" style={styles.bar} />
+      <View style={styles.logoContainer}>
+        <Image source={require("./assets/logo-red.png")} style={styles.logo} />
+        <Text>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.bar}></View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.bar}></View>
+        <View style={[styles.bar, { backgroundColor: "#4ECDC4" }]}></View>
+      </View>
     </ImageBackground>
   );
 }
+
 const styles = StyleSheet.create({
   bar: {
-    width: 100,
+    height: 60,
     backgroundColor: "#fc5c65",
+  },
+  buttonContainer: {
+    width: "100%",
+    height: 120,
+    position: "absolute",
+    bottom: 0,
   },
   image: {
     flex: 1,
@@ -36,5 +45,10 @@ const styles = StyleSheet.create({
   logo: {
     width: 100,
     height: 100,
+  },
+  logoContainer: {
+    flex: 1,
+    alignItems: "center",
+    top: 100,
   },
 });
