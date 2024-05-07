f94519b80116a34caf09644ea20b650d1abff748
paul-mcaviney <paul.mcaviney@protonmail.com>
2024-05-06 18:15:27 -0600

    Finish layout of ViewImageScreen

--- screens/ViewImageScreen.jsx
+++ screens/ViewImageScreen.jsx
@@ -1,8 +1,17 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar />
       <View style={styles.buttonContainer}>
         <View style={styles.button} />
         <View style={[styles.button, { backgroundColor: "#4ECDC4" }]} />
@@ -24,15 +33,19 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    width: "80%",
+    width: "100%",
     flexDirection: "row",
-    alignContent: "center",
+    justifyContent: "space-between",
+    paddingLeft: 20,
+    paddingRight: 20,
   },
   container: {
     backgroundColor: "black",
     flex: 1,
     width: "100%",
     alignItems: "center",
+    paddingTop: 20,
+    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   image: {
     width: "100%",
