5168b5083b6147458c3b4c7c30377ba4f25965c5
paul-mcaviney <paul.mcaviney@protonmail.com>
2024-05-08 07:24:53 -0600

    More learning!

--- App.js
+++ App.js
@@ -1,60 +1,27 @@
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { Text, View } from "react-native";
 
 export default function App() {
-  // return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          shadowColor: "grey",
+          shadowOffset: { width: 10, height: 10 },
+          shadowOpacity: 1,
+          elevation: 20,
+        }}
+      ></View>
+    </View>
+  );
 }
-// import {
-//   Button,
-//   Image,
-//   ImageBackground,
-//   StyleSheet,
-//   Text,
-//   View,
-// } from "react-native";
-
-// export default function App() {
-//   return (
-//     <ImageBackground
-//       source={require("./assets/background.jpg")}
-//       resizeMode="cover"
-//       style={styles.image}
-//     >
-//       <View style={styles.logoContainer}>
-//         <Image source={require("./assets/logo-red.png")} style={styles.logo} />
-//         <Text>Sell What You Don't Need</Text>
-//       </View>
-//       <View style={styles.buttonContainer}>
-//         <View style={styles.bar}></View>
-//         <View style={[styles.bar, { backgroundColor: "#4ECDC4" }]}></View>
-//       </View>
-//     </ImageBackground>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   bar: {
-//     height: 60,
-//     backgroundColor: "#fc5c65",
-//   },
-//   buttonContainer: {
-//     width: "100%",
-//     height: 120,
-//     position: "absolute",
-//     bottom: 0,
-//   },
-//   image: {
-//     flex: 1,
-//     alignItems: "center",
-//   },
-//   logo: {
-//     width: 100,
-//     height: 100,
-//   },
-//   logoContainer: {
-//     flex: 1,
-//     alignItems: "center",
-//     top: 100,
-//   },
-// });
